ebc6978feb889da27ca4c9fe6438621789aa8130
Rafael Rocha <Rafaelgot10@outlook.com>
2023-07-31 19:12:24 -0300

    feat: add User entitie without relation

---
+++ src/entities/users.entities.ts
@@ -0,0 +1,75 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  BeforeInsert,
+  BeforeUpdate,
+} from "typeorm";
+
+import { getRounds, hashSync } from "bcryptjs";
+
+@Entity("users")
+class User {
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+
+  @Column({ type: "varchar" })
+  name: string;
+
+  @Column({ type: "varchar", unique: true })
+  email: string;
+
+  @Column({ type: "varchar" })
+  cpf: string;
+
+  @Column({ type: "varchar" })
+  phone: string;
+
+  @Column({ type: "date" })
+  birth_date: Date;
+
+  @Column({ type: "varchar" })
+  description: string;
+
+  @Column({ type: "varchar" })
+  zip_code: string;
+
+  @Column({ type: "varchar", length: 2 })
+  state: string;
+
+  @Column({ type: "varchar" })
+  city: string;
+
+  @Column({ type: "varchar" })
+  street: string;
+
+  @Column({ type: "varchar" })
+  number: string;
+
+  @Column({ type: "varchar", nullable: true })
+  complement: string;
+
+  @Column({})
+  is_advertise: boolean;
+
+  @Column({ type: "varchar" })
+  password: string;
+
+  @Column({ type: "varchar" })
+  confirm_password: string;
+
+  @CreateDateColumn({ type: "date" })
+  createdAt?: Date | string;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  hash() {
+    const newPassword = getRounds(this.password);
+    if (!newPassword) {
+      this.password = hashSync(this.password, 10);
+    }
+  }
+}
+
+export default User;
