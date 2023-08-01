85c1851e0a002e5823aa6128cf8cb8a518eec840
Rafael Rocha <Rafaelgot10@outlook.com>
2023-08-01 12:49:20 -0300

    fead:add checkParameterUserId middleware

---
+++ src/middlewares/checkParameterUserId.middleware.ts
@@ -0,0 +1,34 @@
+import { Repository } from "typeorm";
+import { AppError } from "../error";
+import { AppDataSource } from "../data-source";
+import { NextFunction, Request, Response } from "express";
+import User from "../entities/users.entities";
+
+const checkParameterUserId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<Response | void> => {
+  const userId = parseInt(req.params.id);
+
+  // if (!userId) {
+  //   userId = res.locals.sub;
+  // }
+
+  const userRepository: Repository<User> = AppDataSource.getRepository(User);
+
+  const user: User | null = await userRepository.findOne({
+    where: {
+      id: userId,
+    },
+  });
+
+  if (!user) {
+    throw new AppError("User not found", 404);
+  }
+
+  res.locals.user = user;
+  return next();
+};
+
+export default checkParameterUserId;
