7fdfcb7ca6765c52c02ed6ec8898cf36061c9886
Rafael Rocha <Rafaelgot10@outlook.com>
2023-08-01 13:44:38 -0300

    fead:add view a user route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   createUserController,
   listAllUserController,
+  listUserbyIdController,
 } from "../controllers/users.controllers";
 import checkRequestBody from "../middlewares/checkRequestBody.middleware";
 import { userSchemaRegister } from "../schemas/users.schemas";
 import checkEmailUser from "../middlewares/checkEmailUser.middleware";
 import ensureTokenExistis from "../middlewares/ensuretokenexistis.middleware";
+import checkParameterUserId from "../middlewares/checkParameterUserId.middleware";
 
 const usersRoutes: Router = Router();
 
@@ -18,5 +20,12 @@ usersRoutes.post(
 );
 
 usersRoutes.get("", ensureTokenExistis, listAllUserController);
+
+usersRoutes.get(
+  "/:id",
+  ensureTokenExistis,
+  checkParameterUserId,
+  listUserbyIdController
+);
 
 export default usersRoutes;
