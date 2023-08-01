91d6deaf4467b87a5b9449464ae5d7b981a7b5fb
Rafael Rocha <Rafaelgot10@outlook.com>
2023-08-01 18:49:45 -0300

    fead:add advert schemas

---
+++ src/schemas/adverts.schemas.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+export const advertSchema = z.object({
+  id: z.number(),
+  brand: z.string(),
+  model: z.string().email(),
+  year: z.string(),
+  fuel: z.string(),
+  mileage: z.string(),
+  color: z.string(),
+  price_FIPE: z.string(),
+  price: z.string(),
+  description: z.string(),
+  cover_image: z.string().nullable(),
+  first_image: z.string().nullable(),
+  second_image: z.string().nullable(),
+  createdAt: z.string(),
+});
+
+export const advertSchemaRequest = advertSchema.omit({
+  id: true,
+  createdAt: true,
+});
+
+export const updateAdvertSchema = advertSchemaRequest.partial();
+
+export const advertsSchema = advertSchema.array();
