4f8dbb1f4d8cf57aa98c1672a00d3b30c3241c49
Rafael Rocha <Rafaelgot10@outlook.com>
2023-08-02 11:28:39 -0300

    feat:add create advert service

---
+++ src/Services/adverts/createAdverts.service.ts
@@ -0,0 +1,49 @@
+import { Repository } from "typeorm";
+import { AppDataSource } from "../../data-source";
+import User from "../../entities/users.entities";
+import {
+  TAdvertRequest,
+  TAdvertResponse,
+} from "../../interfaces/adverts.interfaces";
+import { userSchemaResponse } from "../../schemas/users.schemas";
+import { TUser, TUserResponse } from "../../interfaces/users.interfaces";
+import Advert from "../../entities/adverts.entities";
+import { advertSchemaResponse } from "../../schemas/adverts.schemas";
+import { AppError } from "../../error";
+
+const createAdvertService = async (
+  contactData: TAdvertRequest,
+  userId: number
+): Promise<TAdvertResponse> => {
+  const userRepository: Repository<User> = AppDataSource.getRepository(User);
+
+  const user: User | null = await userRepository.findOne({
+    where: {
+      id: userId,
+    },
+  });
+
+  if (!user) {
+    throw new AppError("User not Found", 404);
+  }
+
+  const foundUser: TUser = userSchemaResponse.parse(user);
+
+  const newAdvertData = {
+    ...contactData,
+    user: foundUser,
+  };
+
+  const advertRepository: Repository<Advert> =
+    AppDataSource.getRepository(Advert);
+
+  const advert: Advert = advertRepository.create(newAdvertData);
+
+  await advertRepository.save(advert);
+
+  const returnAdvert = advertSchemaResponse.parse(advert);
+
+  return returnAdvert;
+};
+
+export default createAdvertService;
--- src/interfaces/adverts.interfaces.ts
+++ src/interfaces/adverts.interfaces.ts
@@ -2,11 +2,14 @@ import { z } from "zod";
 import {
   advertSchema,
   advertSchemaRequest,
+  advertSchemaResponse,
   advertsSchema,
   updateAdvertSchema,
 } from "../schemas/adverts.schemas";
 
 export type TAdvert = z.infer<typeof advertSchema>;
+
+export type TAdvertResponse = z.infer<typeof advertSchemaResponse>;
 
 export type TAdvertRequest = z.infer<typeof advertSchemaRequest>;
 
